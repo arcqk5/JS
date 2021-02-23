683132ad25f9e228cdb947982794717166b2c01e
arcqk5 <grigoriylafutkin@yandex.ru>
2021-02-23 16:18:42 +0300

    added fix-header

---
+++ js/fix-header.js
@@ -0,0 +1,16 @@
+let lastScroll = 0;
+const header = document.querySelector('.header');
+
+const scrollPosition = () => window.pageYOffset || document.documentElement.scrollTop;
+const containHide = () => header.classList.contains('hide');
+
+window.addEventListener('scroll', () => {
+    if(scrollPosition() > lastScroll && !containHide()) {
+        header.classList.add('hide');
+    }
+    else if(scrollPosition() < lastScroll && containHide()){
+        header.classList.remove('hide');
+    }
+
+    lastScroll = scrollPosition();
+})
