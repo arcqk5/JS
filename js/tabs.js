f6385fa6dc5f5ba7e71c6cdb80f827b303abdcd0
arcqk5 <grigoriylafutkin@yandex.ru>
2021-02-24 22:31:22 +0300

    add tabs js

---
+++ js/tabs.js
@@ -0,0 +1,27 @@
+let tab = function() {
+    let tabNav = document.querySelectorAll('.tabs--bar__item'),
+        tabContent = document.querySelectorAll('.tab'),
+        tabName;
+
+    tabNav.forEach(item => {
+        item.addEventListener('click', selectTabNav)
+    });
+
+    function selectTabNav() {
+        tabNav.forEach(item => {
+            item.classList.remove('active');
+        });
+        this.classList.add('active');
+        tabName = this.getAttribute('data-tab-name');
+        selectTabContent(tabName);
+    }
+
+    function selectTabContent(tabName) {
+        tabContent.forEach(item => {
+            item.classList.contains(tabName) ? item.classList.add('active') : item.classList.remove('active');
+        });
+    }
+};
+
+
+tab();
